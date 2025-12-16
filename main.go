f070e4b37e0faf4af288510ec4566ba3aea6d636
Hayden Braxton <hayden.braxton@flocksafety.com>
2025-12-16 14:48:31 -0500

    initial commit

---
+++ main.go
@@ -0,0 +1,247 @@
+package main
+
+import (
+	"bytes"
+	"encoding/xml"
+	"fmt"
+	"io"
+	"log/slog"
+	"net/http"
+	"os"
+	"strconv"
+	"strings"
+
+	"github.com/gin-gonic/gin"
+)
+
+const (
+	port            = 8081
+	soapNamespace   = "http://www.w3.org/2003/05/soap-envelope"
+	tdsNamespace    = "http://www.onvif.org/ver10/device/wsdl"
+	media2Namespace = "http://www.onvif.org/ver20/media/wsdl"
+	ttNamespace     = "http://www.onvif.org/ver10/schema"
+	soapContentType = "application/soap+xml; charset=utf-8"
+)
+
+var appLogger = slog.New(slog.NewTextHandler(os.Stdout, nil))
+
+func main() {
+	gin.SetMode(gin.ReleaseMode)
+
+	router := gin.New()
+	router.Use(gin.Recovery())
+	router.Use(requestLogger())
+	router.POST("/onvif/device_service", deviceServiceHandler())
+
+	router.NoRoute(func(c *gin.Context) {
+		appLogger.Warn("NoRoute hit", "method", c.Request.Method, "path", c.Request.URL.Path)
+		c.Status(http.StatusInternalServerError)
+	})
+
+	addr := ":" + strconv.Itoa(port)
+	appLogger.Info("HTTP request logger listening", "addr", addr)
+
+	if err := router.Run(addr); err != nil {
+		appLogger.Error("server stopped", "err", err)
+		os.Exit(1)
+	}
+}
+
+// requestLogger logs request/response metadata and bodies for quick inspection.
+func requestLogger() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		var body []byte
+		if c.Request.Body != nil {
+			body, _ = io.ReadAll(c.Request.Body)
+			c.Request.Body = io.NopCloser(bytes.NewBuffer(body))
+		}
+
+		blw := &bodyLogWriter{ResponseWriter: c.Writer}
+		c.Writer = blw
+
+		c.Next()
+
+		appLogger.Info(
+			"request completed",
+			"method", c.Request.Method,
+			"path", c.Request.URL.Path,
+			"client", c.ClientIP(),
+			"query", c.Request.URL.RawQuery,
+			"headers", c.Request.Header,
+			"request_body", string(body),
+			"status", c.Writer.Status(),
+			"response_size", c.Writer.Size(),
+			"response_body", blw.body.String(),
+		)
+	}
+}
+
+type soapEnvelope struct {
+	Body struct {
+		Raw string `xml:",innerxml"`
+	} `xml:"Body"`
+}
+
+type bodyLogWriter struct {
+	gin.ResponseWriter
+	body bytes.Buffer
+}
+
+func (w *bodyLogWriter) Write(b []byte) (int, error) {
+	w.body.Write(b)
+	return w.ResponseWriter.Write(b)
+}
+
+func (w *bodyLogWriter) WriteString(s string) (int, error) {
+	w.body.WriteString(s)
+	return w.ResponseWriter.WriteString(s)
+}
+
+func deviceServiceHandler() gin.HandlerFunc {
+	const (
+		getServicesAction          = "GetServices"
+		getCapabilitiesAction      = "GetCapabilities"
+		getNetworkInterfacesAction = "GetNetworkInterfaces"
+		getDeviceInfoAction        = "GetDeviceInformation"
+		getUsersAction             = "GetUsers"
+	)
+
+	return func(c *gin.Context) {
+		var envelope soapEnvelope
+		if err := xml.NewDecoder(c.Request.Body).Decode(&envelope); err != nil {
+			appLogger.Warn("failed to parse device request", "err", err)
+			c.Status(http.StatusBadRequest)
+			return
+		}
+
+		scheme := requestScheme(c)
+		host := c.Request.Host
+		if host == "" {
+			host = "localhost:" + strconv.Itoa(port)
+		}
+
+		bodyContent := strings.TrimSpace(envelope.Body.Raw)
+		switch {
+		case strings.Contains(bodyContent, getServicesAction):
+			payload := buildGetServicesResponse(scheme, host)
+			c.Data(http.StatusOK, soapContentType, []byte(payload))
+		case strings.Contains(bodyContent, getCapabilitiesAction):
+			payload := buildGetCapabilitiesResponse(scheme, host)
+			c.Data(http.StatusOK, soapContentType, []byte(payload))
+		case strings.Contains(bodyContent, getNetworkInterfacesAction):
+			payload := buildGetNetworkInterfacesResponse()
+			c.Data(http.StatusOK, soapContentType, []byte(payload))
+		case strings.Contains(bodyContent, getDeviceInfoAction):
+			payload := buildGetDeviceInformationResponse()
+			c.Data(http.StatusOK, soapContentType, []byte(payload))
+		case strings.Contains(bodyContent, getUsersAction):
+			payload := buildGetUsersResponse()
+			c.Data(http.StatusOK, soapContentType, []byte(payload))
+		default:
+			appLogger.Warn("device_service request action not recognized", "body", bodyContent)
+			c.Status(http.StatusBadRequest)
+		}
+	}
+}
+
+func buildGetServicesResponse(scheme, host string) string {
+	xAddr := fmt.Sprintf("%s://%s/onvif/media2_service", scheme, host)
+
+	return fmt.Sprintf(`<?xml version="1.0" encoding="UTF-8"?>
+<s:Envelope xmlns:s="%s">
+	<s:Body>
+		<tds:GetServicesResponse xmlns:tds="%s" xmlns:tt="%s">
+			<tds:Service>
+				<tds:Namespace>%s</tds:Namespace>
+				<tds:XAddr>%s</tds:XAddr>
+				<tds:Version>
+					<tt:Major>2</tt:Major>
+					<tt:Minor>0</tt:Minor>
+				</tds:Version>
+			</tds:Service>
+		</tds:GetServicesResponse>
+	</s:Body>
+</s:Envelope>`, soapNamespace, tdsNamespace, ttNamespace, media2Namespace, xAddr)
+}
+
+func buildGetCapabilitiesResponse(scheme, host string) string {
+	deviceXAddr := fmt.Sprintf("%s://%s/onvif/device_service", scheme, host)
+	mediaXAddr := fmt.Sprintf("%s://%s/onvif/media2_service", scheme, host)
+
+	return fmt.Sprintf(`<?xml version="1.0" encoding="UTF-8"?>
+<s:Envelope xmlns:s="%s">
+	<s:Body>
+		<tds:GetCapabilitiesResponse xmlns:tds="%s" xmlns:tt="%s">
+			<tds:Capabilities>
+				<tt:Device>
+					<tt:XAddr>%s</tt:XAddr>
+				</tt:Device>
+				<tt:Media>
+					<tt:XAddr>%s</tt:XAddr>
+					<tt:StreamingCapabilities>
+						<tt:RTPMulticast>false</tt:RTPMulticast>
+						<tt:RTP_TCP>true</tt:RTP_TCP>
+						<tt:RTP_RTSP_TCP>true</tt:RTP_RTSP_TCP>
+					</tt:StreamingCapabilities>
+				</tt:Media>
+			</tds:Capabilities>
+		</tds:GetCapabilitiesResponse>
+	</s:Body>
+</s:Envelope>`, soapNamespace, tdsNamespace, ttNamespace, deviceXAddr, mediaXAddr)
+}
+
+func buildGetNetworkInterfacesResponse() string {
+	return fmt.Sprintf(`<?xml version="1.0" encoding="UTF-8"?>
+<s:Envelope xmlns:s="%s">
+	<s:Body>
+		<tds:GetNetworkInterfacesResponse xmlns:tds="%s" xmlns:tt="%s">
+			<tds:NetworkInterfaces/>
+		</tds:GetNetworkInterfacesResponse>
+	</s:Body>
+</s:Envelope>`, soapNamespace, tdsNamespace, ttNamespace)
+}
+
+func buildGetDeviceInformationResponse() string {
+	return fmt.Sprintf(`<?xml version="1.0" encoding="UTF-8"?>
+<s:Envelope xmlns:s="%s">
+	<s:Body>
+		<tds:GetDeviceInformationResponse xmlns:tds="%s" xmlns:tt="%s">
+			<tds:Manufacturer>Flock Safety</tds:Manufacturer>
+			<tds:Model>Condor</tds:Model>
+			<tds:FirmwareVersion>v1.0</tds:FirmwareVersion>
+			<tds:SerialNumber>serialnumber123</tds:SerialNumber>
+			<tds:HardwareId>hardwareid123</tds:HardwareId>
+		</tds:GetDeviceInformationResponse>
+	</s:Body>
+</s:Envelope>`, soapNamespace, tdsNamespace, ttNamespace)
+}
+
+func buildGetUsersResponse() string {
+	return fmt.Sprintf(`<?xml version="1.0" encoding="UTF-8"?>
+<s:Envelope xmlns:s="%s">
+	<s:Body>
+		<tds:GetUsersResponse xmlns:tds="%s" xmlns:tt="%s">
+			<tds:User>
+				<tt:Username>flock</tt:Username>
+				<tt:UserLevel>User</tt:UserLevel>
+			</tds:User>
+		</tds:GetUsersResponse>
+	</s:Body>
+</s:Envelope>`, soapNamespace, tdsNamespace, ttNamespace)
+}
+
+func requestScheme(c *gin.Context) string {
+	if proto := c.GetHeader("X-Forwarded-Proto"); proto != "" {
+		return proto
+	}
+
+	if c.Request.TLS != nil {
+		return "https"
+	}
+
+	if c.Request.URL != nil && c.Request.URL.Scheme != "" {
+		return c.Request.URL.Scheme
+	}
+
+	return "http"
+}
